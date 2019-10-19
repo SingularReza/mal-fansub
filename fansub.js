45807eb5f702da099d50dd90ed5b881085ce7414
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-10-19 10:46:40 +0530

    api now works in content script

--- fansub.js
+++ fansub.js
@@ -1,7 +1,3 @@
-window.hidesubs = false;
-
-var id = documet.getElementById("myinfo_anime_id").value;
-
 var heading = document.createElement("h2");
 heading.appendChild(document.createTextNode("Fansubs"));
 document.getElementsByClassName("pb24")[0].appendChild(document.createElement("br"));
@@ -10,59 +6,26 @@ document.getElementsByClassName("pb24")[0].appendChild(heading);
 var loader = document.createElement("a");
 loader.setAttribute('href', "javascript:void(0)");
 loader.innerText = "loading...";
-
-fetch("https://gudnight.me/api/?id=" + id).
-    then(data => data.text()).
-    then(function(data) {
-        loader.style.textDecoration = "none";
-        loader.style.color = "black";
-        loader.innerHTML = "";
-
-        var checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = 'hidelang';
-        checkbox.onclick = function()
-        {
-            var elements = document.getElementsByClassName("nonenglish");
-
-            if(checkbox.checked) {
-                for (var x = 0; x < elements.length; x++)
-                { elements[x].style.display = "none"; }
-            } else {
-                for (var y = 0; y < elements.length; y++)
-                { elements[y].style.display = "block"; }
-            }
-        };
-
-        var newlabel = document.createElement("Label");
-        newlabel.setAttribute("for", "hidelang");
-        newlabel.innerHTML = "Hide non-English subs";
-
-        loader.appendChild(checkbox);
-        loader.appendChild(newlabel);
-
-        var htmlresponse = document.createElement('div');
-        htmlresponse.innerHTML = data.trim();
-
-        var links = htmlresponse.getElementsByClassName("commentToggle");
-
-        for (var i = 0; i < links.length; i++) {
-            links[i].onclick = function()
-            {
-                var box = document.getElementById("comments" + this.e);
-                var style = box.currentStyle ? box.currentStyle.display : getComputedStyle(box, null).display;
-
-                if(style === "none") box.style.display = "block";
-                else box.style.display = "none";
-            };
-            links[i].e = links[i].id;
-        }
-
-        document.getElementsByClassName("pb24")[0].appendChild(htmlresponse);
-
-        if(window.hidesubs) checkbox.click();
-    });
-
 document.getElementsByClassName("pb24")[0].appendChild(loader);
-document.getElementsByClassName("pb24")[0].appendChild(document.createElement("br"));
-document.getElementsByClassName("pb24")[0].appendChild(document.createElement("br"));
+
+document.body.style.border = "5px solid red";
+var id = document.getElementById("myinfo_anime_id").value;
+console.log(id)
+var url = "http://localhost:3300/api/fangroups/"+id;
+
+function createGroup(groupdata) {
+    var group = document.createElement("div");
+    group.appendChild(document.createTextNode(groupdata.groupname));
+    group.appendChild(document.createTextNode(groupdata.approve_line));
+    document.getElementsByClassName("pb24")[0].appendChild(group);
+}
+
+fetch(url)
+    .then(res => {
+       res.json()
+        .then(data => {
+            console.log(data);
+            loader.innerHTML = "";
+            data.forEach((item)=>createGroup(item));
+        })
+    });
