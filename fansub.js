35161ea1a766bb1990a5b4feb2a8029aa106c87a
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-10-21 21:17:49 +0530

    added comments about fansubs

--- fansub.js
+++ fansub.js
@@ -11,12 +11,31 @@ document.getElementsByClassName("pb24")[0].appendChild(loader);
 document.body.style.border = "5px solid red";
 var id = document.getElementById("myinfo_anime_id").value;
 console.log(id)
-var url = "http://localhost:3300/api/fangroups/"+id;
+var url = "http://localhost:3300/api/anime/"+id;
+
+function showComments(e) {
+    console.log(e.target.id);
+    var groupid = e.target.id;
+    fetch("http://localhost:3300/api/comments/" + id + groupid)
+        .then(res => {
+            res.json()
+                .then(data => {
+                    console.log(data);
+                    loader.innerHTML = "";
+                    var group = document.getElementsByClassName(groupid) ;
+                    data.forEach((item) => createComment(item, group));
+                })
+        });
+
+}
 
 function createGroup(groupdata) {
     var group = document.createElement("div");
     group.appendChild(document.createTextNode(groupdata.groupname));
     group.appendChild(document.createTextNode(groupdata.approve_line));
+    group.appendChild(document.createTextNode(groupdata.language));
+    group.id = groupdata.groupid;
+    group.onclick = showComments;
     document.getElementsByClassName("pb24")[0].appendChild(group);
 }
 
@@ -26,6 +45,7 @@ fetch(url)
         .then(data => {
             console.log(data);
             loader.innerHTML = "";
+            //pagedata = data;
             data.forEach((item)=>createGroup(item));
         })
     });
